1b8acb35efa7b78159ca1961ae6e61533c75af4d
Guraba <kondrik2178@gmail.com>
2026-03-14 02:34:39 -0700

    Cimm

--- index.jsx
+++ index.jsx
@@ -1,2 +1,18 @@
-queueMicrotask;
-webkitURL
+// App.jsx
+function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        {/* Здесь будет ваш контент */}
+        <h1>Hello React!</h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
